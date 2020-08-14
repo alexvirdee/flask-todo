996bc64dbbc30f05676e0ccfcf069f55d2142f4c
Alexinder Virdee <alex.virdee08@gmail.com>
2020-08-14 13:45:30 -0400

    Dev server

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask 
 
-app = Flas(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return "Hello World"
+
+if __name__ == "__main__":
+    app.run(debug=True)
